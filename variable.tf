5c3bc9651527502217f2893dbbab374e7e7cbccc
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-23 15:29:43 +0530

    Updated variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,5 @@
 variable "region" {
   description = "The AWS region to deploy resources"
+  type        = string
   default     = "us-east-1"
-}
-
-variable "s3_bucket" {
-  description = "The name of the source S3 bucket"
-  type        = string
-  default     = "source-images-bucket-273550"
-}
-
-variable "destination_bucket" {
-  description = "The name of the destination S3 bucket"
-  type        = string
-  default     = "processed-images-bucket-273550"
 }
